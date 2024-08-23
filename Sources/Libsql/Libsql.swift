ad79230483d5301205d81211aa0a0ace1baeb3c6
Levy A. <levyddsa@gmail.com>
2024-08-23 16:45:20 -0300

    add Statement + more complex querying

--- Sources/Libsql/Libsql.swift
+++ Sources/Libsql/Libsql.swift
@@ -6,10 +6,15 @@ enum Value {
     case text(String)
     case blob(Data)
     case real(Double)
+    case null
 }
 
 protocol ValueRepresentable {
     func toValue() -> Value
+}
+
+extension Value: ValueRepresentable {
+    func toValue() -> Value { self }
 }
 
 extension Int: ValueRepresentable {
@@ -30,6 +35,16 @@ extension Data: ValueRepresentable {
 
 extension Double: ValueRepresentable {
     func toValue() -> Value { .real(self) }
+}
+
+extension String? {
+    func withCString<Result>(_ body: (UnsafePointer<Int8>?) throws -> Result) rethrows -> Result {
+        if self == nil {
+            return try body(nil)
+        } else {
+            return try self!.withCString(body)
+        }
+    }
 }
 
 enum LibsqlError: Error {
@@ -57,6 +72,8 @@ class Row {
             throw LibsqlError.runtimeError(String(cString: err!))
         }
 
+        defer { libsql_free_blob(slice) }
+
         return Data(bytes: slice.ptr, count: Int(slice.len))
     }
 
@@ -81,6 +98,8 @@ class Row {
             throw LibsqlError.runtimeError(String(cString: err!))
         }
 
+        defer { libsql_free_string(string) }
+
         return String(cString: string!)
     }
 
@@ -96,7 +115,7 @@ class Row {
     }
 }
 
-class Rows {
+class Rows: Sequence, IteratorProtocol {
     var inner: libsql_rows_t
 
     fileprivate init(fromPtr inner: libsql_rows_t) {
@@ -107,17 +126,119 @@ class Rows {
         libsql_free_rows(self.inner)
     }
 
-    func next() throws -> Row? {
+    func next() -> Row? {
         var row: libsql_row_t?
 
         var err: UnsafePointer<CChar>?
-        if libsql_next_row(self.inner, &row, &err) != 0 {
+        guard libsql_next_row(self.inner, &row, &err) == 0 else {
             defer { libsql_free_string(err) }
-            throw LibsqlError.runtimeError(String(cString: err!))
+            fatalError(String(cString: err!))
         }
 
         return Row(fromPtr: row)
 
+    }
+}
+
+class Statement {
+    var inner: libsql_stmt_t
+
+    deinit {
+        libsql_free_stmt(self.inner)
+    }
+
+    fileprivate init(fromPtr inner: libsql_stmt_t) {
+        self.inner = inner
+    }
+
+    func execute() throws {
+        var err: UnsafePointer<CChar>? = nil
+        if libsql_execute_stmt(self.inner, &err) != 0 {
+            defer { libsql_free_string(err) }
+            throw LibsqlError.runtimeError(String(cString: err!))
+        }
+    }
+
+    func execute(_ params: [ValueRepresentable]) throws {
+        try self.bind(params)
+        return try self.execute()
+    }
+
+    func execute(_ params: ValueRepresentable...) throws {
+        try self.bind(params)
+        return try self.execute()
+    }
+
+    func query() throws -> Rows {
+        var rows: libsql_rows_t? = nil
+
+        var err: UnsafePointer<CChar>? = nil
+        if libsql_query_stmt(self.inner, &rows, &err) != 0 {
+            defer { libsql_free_string(err) }
+            throw LibsqlError.runtimeError(String(cString: err!))
+        }
+
+        return Rows(fromPtr: rows!)
+    }
+
+    func query(_ params: [ValueRepresentable]) throws -> Rows {
+        try self.bind(params)
+        return try self.query()
+    }
+
+    func query(_ params: ValueRepresentable...) throws -> Rows {
+        try self.bind(params)
+        return try self.query()
+    }
+
+    func bind(_ params: ValueRepresentable...) throws {
+        return try self.bind(params)
+    }
+
+    func bind(_ params: [ValueRepresentable]) throws {
+        for (i, v) in params.enumerated() {
+            let i = Int32(i + 1)
+
+            switch v.toValue() {
+            case .integer(let integer):
+                var err: UnsafePointer<CChar>? = nil
+                if libsql_bind_int(self.inner, i, integer, &err) != 0 {
+                    defer { libsql_free_string(err) }
+                    throw LibsqlError.runtimeError(String(cString: err!))
+                }
+            case .text(let text):
+                try text.withCString { text in
+                    var err: UnsafePointer<CChar>? = nil
+                    if libsql_bind_string(self.inner, i, text, &err) != 0 {
+                        defer { libsql_free_string(err) }
+                        throw LibsqlError.runtimeError(String(cString: err!))
+                    }
+                }
+            case .blob(let slice):
+                try slice.withUnsafeBytes { slice in
+                    let base = slice.baseAddress?.assumingMemoryBound(to: UInt8.self)
+                    let len = Int32(slice.count)
+
+                    var err: UnsafePointer<CChar>? = nil
+                    if libsql_bind_blob(self.inner, i, base, len, &err) != 0 {
+                        defer { libsql_free_string(err) }
+                        throw LibsqlError.runtimeError(String(cString: err!))
+                    }
+                }
+            case .real(let real):
+                var err: UnsafePointer<CChar>? = nil
+                if libsql_bind_float(self.inner, i, real, &err) != 0 {
+                    defer { libsql_free_string(err) }
+                    throw LibsqlError.runtimeError(String(cString: err!))
+                }
+            case .null:
+                var err: UnsafePointer<CChar>? = nil
+                if libsql_bind_null(self.inner, i, &err) != 0 {
+                    defer { libsql_free_string(err) }
+                    throw LibsqlError.runtimeError(String(cString: err!))
+                }
+            }
+        }
     }
 }
 
@@ -145,6 +266,15 @@ class Connection {
         return Rows(fromPtr: rows!)
     }
 
+    func query(_ sql: String, _ params: [ValueRepresentable]) throws -> Rows {
+        let stmt = try self.prepare(sql)
+        return try stmt.query(params)
+    }
+
+    func query(_ sql: String, _ params: ValueRepresentable...) throws -> Rows {
+        return try self.query(sql, params as [ValueRepresentable])
+    }
+
     func execute(_ sql: String) throws {
         try sql.withCString { sql in
             var err: UnsafePointer<CChar>? = nil
@@ -155,7 +285,16 @@ class Connection {
         }
     }
 
+    func execute(_ sql: String, _ params: [ValueRepresentable]) throws {
+        let stmt = try self.prepare(sql)
+        return try stmt.execute(params)
+    }
+
     func execute(_ sql: String, _ params: ValueRepresentable...) throws {
+        return try self.execute(sql, params as [ValueRepresentable])
+    }
+
+    func prepare(_ sql: String) throws -> Statement {
         var stmt: libsql_stmt_t? = nil
 
         try sql.withCString { sql in
@@ -166,42 +305,7 @@ class Connection {
             }
         }
 
-        for (i, v) in params.enumerated() {
-            let i = Int32(i + 1)
-
-            switch v.toValue() {
-            case .integer(let integer):
-                var err: UnsafePointer<CChar>? = nil
-                if libsql_bind_int(stmt, i, integer, &err) != 0 {
-                    defer { libsql_free_string(err) }
-                    throw LibsqlError.runtimeError(String(cString: err!))
-                }
-            case .text(let text):
-                try text.withCString { text in
-                    var err: UnsafePointer<CChar>? = nil
-                    if libsql_bind_string(stmt, i, text, &err) != 0 {
-                        defer { libsql_free_string(err) }
-                        throw LibsqlError.runtimeError(String(cString: err!))
-                    }
-                }
-            case .blob(let blob):
-                try blob.withUnsafeBytes { blob in
-                    let blob = blob.baseAddress?.assumingMemoryBound(to: UInt8.self)
-                    var err: UnsafePointer<CChar>? = nil
-                    if libsql_bind_string(stmt, i, blob, &err) != 0 {
-                        defer { libsql_free_string(err) }
-                        throw LibsqlError.runtimeError(String(cString: err!))
-                    }
-                }
-            case .real(let real):
-                var err: UnsafePointer<CChar>? = nil
-                if libsql_bind_float(stmt, i, real, &err) != 0 {
-                    defer { libsql_free_string(err) }
-                    throw LibsqlError.runtimeError(String(cString: err!))
-                }
-            }
-        }
-
+        return Statement(fromPtr: stmt!)
     }
 }
 
@@ -212,10 +316,18 @@ class Database {
         libsql_close(self.inner)
     }
 
+    func sync() throws {
+        var err: UnsafePointer<CChar>? = nil
+        if libsql_sync(self.inner, &err) != 0 {
+            defer { libsql_free_string(err) }
+            throw LibsqlError.runtimeError(String(cString: err!))
+        }
+    }
+
     func connect() throws -> Connection {
         var conn: libsql_connection_t? = nil
-        var err: UnsafePointer<CChar>? = nil
 
+        var err: UnsafePointer<CChar>? = nil
         if libsql_connect(self.inner, &conn, &err) != 0 {
             defer { libsql_free_string(err) }
             throw LibsqlError.runtimeError(String(cString: err!))
@@ -226,9 +338,9 @@ class Database {
 
     init(_ path: String) throws {
         var db: libsql_database_t? = nil
-        var err: UnsafePointer<CChar>? = nil
 
         try path.withCString { path in
+            var err: UnsafePointer<CChar>? = nil
             if libsql_open_ext(path, &db, &err) != 0 {
                 defer { libsql_free_string(err) }
                 throw LibsqlError.runtimeError(String(cString: err!))
@@ -238,15 +350,63 @@ class Database {
         self.inner = db!
     }
 
-    init(url: String, authToken: String) throws {
+    init(url: String, authToken: String, withWebpki: Bool = false) throws {
         var db: libsql_database_t? = nil
-        var err: UnsafePointer<CChar>? = nil
 
         try url.withCString { url in
             try authToken.withCString { authToken in
-                if libsql_open_remote(url, authToken, &db, &err) != 0 {
-                    defer { libsql_free_string(err) }
-                    throw LibsqlError.runtimeError(String(cString: err!))
+                if withWebpki {
+                    var err: UnsafePointer<CChar>? = nil
+                    if libsql_open_remote(url, authToken, &db, &err) != 0 {
+                        defer { libsql_free_string(err) }
+                        throw LibsqlError.runtimeError(String(cString: err!))
+                    }
+                } else {
+                    var err: UnsafePointer<CChar>? = nil
+                    if libsql_open_remote_with_webpki(url, authToken, &db, &err) != 0 {
+                        defer { libsql_free_string(err) }
+                        throw LibsqlError.runtimeError(String(cString: err!))
+                    }
+                }
+            }
+        }
+
+        self.inner = db!
+    }
+
+    init(
+        path: String,
+        url: String,
+        authToken: String,
+        readYourWrites: Bool = false,
+        encryptionKey: String? = nil,
+        syncInterval: Int = 0,
+        withWebpki: Bool = false
+    ) throws {
+        var db: libsql_database_t? = nil
+
+        try path.withCString { path in
+            try url.withCString { url in
+                try authToken.withCString { authToken in
+                    try encryptionKey.withCString { encryptionKey in
+                        var err: UnsafePointer<CChar>? = nil
+                        if libsql_open_sync_with_config(
+                            libsql_config(
+                                db_path: path,
+                                primary_url: url,
+                                auth_token: authToken,
+                                read_your_writes: Int8(readYourWrites ? 1 : 0 as Int),
+                                encryption_key: encryptionKey,
+                                sync_interval: Int32(syncInterval),
+                                with_webpki: Int8(readYourWrites ? 1 : 0 as Int)
+                            ),
+                            &db,
+                            &err
+                        ) != 0 {
+                            defer { libsql_free_string(err) }
+                            throw LibsqlError.runtimeError(String(cString: err!))
+                        }
+                    }
                 }
             }
         }
--- Tests/LibsqlTests/LibsqlTests.swift
+++ Tests/LibsqlTests/LibsqlTests.swift
@@ -1,30 +1,59 @@
+import Foundation
 import XCTest
 
 @testable import Libsql
 
 final class LibsqlTests: XCTestCase {
-    func testOpenDb() throws {
-        _ = try Database(":memory:")
+    func testOpenDbMemory() throws {
+        let db = try Database(":memory:")
+        let conn = try db.connect()
+    }
+    
+    func testOpenDbFile() throws {
+        let db = try Database("test.db")
+        let conn = try db.connect()
     }
 
     func testExecute() throws {
         let db = try Database(":memory:")
         let conn = try db.connect()
-        try conn.execute("create table test (data integer)")
+        try conn.execute("create table test (i integer, s text)")
+        try conn.execute("insert into test values (?, ?)", 1, "lorem ipsum")
     }
 
-    func testQuery() throws {
+    func testQuerySimple() throws {
         let db = try Database(":memory:")
         let conn = try db.connect()
-        print(try conn.query("select 1").next()!.getInt(0))
+        XCTAssertEqual(try conn.query("select 1").next()!.getInt(0), 1)
+        XCTAssertEqual(try conn.query("select ?", 1).next()!.getInt(0), 1)
     }
 
-    func testExecuteParams() throws {
+    func testStatement() throws {
         let db = try Database(":memory:")
         let conn = try db.connect()
-        try conn.execute("create table test (data integer)")
-        try conn.execute("insert into test values (?)", 1)
-        try conn.execute("insert into test values (?)", 2)
-        try conn.execute("insert into test values (?)", 3)
+        let stmt = try conn.prepare("select ?")
+        try stmt.bind(1)
+        XCTAssertEqual(try stmt.query().next()!.getInt(0), 1)
+    }
+
+    func testQueryMultiple() throws {
+        let db = try Database(":memory:")
+        let conn = try db.connect()
+
+        try conn.execute("create table test (i integer, t text, r real, b blob)")
+
+        let range = 0...255
+
+        for i in range {
+            try conn.execute(
+                "insert into test values (?, ?, ?, ?)", i, "\(i)", exp(Double(i)), Data([UInt8(i)]))
+        }
+
+        for (i, row) in zip(range, try conn.query("select * from test")) {
+            XCTAssertEqual(try row.getInt(0), i)
+            XCTAssertEqual(try row.getString(1), "\(i)")
+            XCTAssertEqual(try row.getDouble(2), exp(Double(i)))
+            XCTAssertEqual(try row.getData(3), Data([UInt8(i)]))
+        }
     }
 }
